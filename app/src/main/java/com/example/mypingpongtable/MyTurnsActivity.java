75e92ba9a4011a8960020e718693ae3cdf1f71e8
Ran Hadar <ran.hadar@gmail.com>
2020-06-20 13:19:30 +0300

    Handling MyTurnsActivity

--- app/src/main/java/com/example/mypingpongtable/MyTurnsActivity.java
+++ app/src/main/java/com/example/mypingpongtable/MyTurnsActivity.java
@@ -1,42 +1,59 @@
 package com.example.mypingpongtable;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.github.florent37.expansionpanel.ExpansionHeader;
-import com.github.florent37.expansionpanel.ExpansionLayout;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 
 public class MyTurnsActivity extends AppCompatActivity implements Serializable {
 
+    private TextView welcomePlayerTxt;
+    private Button returnBtn;
+    private String username;
+    private ArrayList<Game> gameList;
+    private ArrayList<Game> deletedGameList;
+    RecyclerView myTurnsRecyclerView;
+    MyTurnAdapter myTurnAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-
-
     }
 
-    private ArrayList<String> getMyList() {
-        ArrayList<String> turns = new ArrayList<>();
+    private ArrayList<MyTurnSlot> getMyList() {
 
+        ArrayList<MyTurnSlot> turns = new ArrayList<>();
+        for (Game game : gameList) {
+            MyTurnSlot slot = new MyTurnSlot();
+            slot.setTurnTime(game.getDateString() + " at " + game.getTimeString());
+
+            if (game.isFull()) {
+                String opponent =
+                        username.equals(game.getPlayer1()) ? game.getPlayer2() : game.getPlayer1();
+                slot.setTurnAgainst("Playing against: " + opponent);
+
+//                slot.setSlotImage(R.drawable.waiting);
+            } else {
+                slot.setTurnAgainst("Waiting for an opponent");
+//                slot.setSlotImage(R.drawable.header_bg);
+            }
+            turns.add(slot);
+        }
         return turns;
-
     }
 
 
+    public void removeGame(int position)
+    {
 
+
+    }
 
 
 }
